2b41b25dba7a403b005b7f166280e0acff22ca2b
Kelvina <Kelvina-web.com>
2025-05-18 12:49:45 +0300

    Adding files

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,30 @@
-def write_file(file_name, file_content):
-    pass
+ def write_file(file_name, file_content):
+    """
+    Writes content to a .txt file
+    Args:
+        file_name (str): name/path of file (without extension)
+        file_content (str): content to write to file
+    """
+    with open(f"{file_name}.txt", mode='w', encoding='utf-8') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    """
+    Appends content to an existing .txt file
+    Args:
+        file_name (str): name/path of file (without extension)
+        append_content (str): content to append to file
+    """
+    with open(f"{file_name}.txt", mode='a', encoding='utf-8') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    """
+    Reads content from a .txt file
+    Args:
+        file_name (str): name/path of file (without extension)
+    Returns:
+        str: content of the file
+    """
+    with open(f"{file_name}.txt", encoding='utf-8') as file:
+        return file.read()
